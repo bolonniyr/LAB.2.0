a851078099a1cd1d6a2b49b6234ac4c1f4707ec7
bolonniyr <147802822+bolonniyr@users.noreply.github.com>
2023-11-02 10:58:34 +0200

    Add files via upload

---
+++ LABOR.2.1/2.1.cpp
@@ -0,0 +1,26 @@
+// Lab_02.1cpp
+	// Болонний Роман
+	// Лабораторна робота № 2.1
+	// Лінійні програми.
+	// Варіант 
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	double a;
+	double z1;
+	double z2;
+	cout << "a = "; cin >> a;
+
+	z1 = sin(a) + sin(5 * a) - sin(3 * a) / cos(a) - cos(3 * a) + cos(5 * a);
+	z2 = tan(3 * a);
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
